74acb197446bc5d595ee2bab5c67b557656ebed6
sweetpatry <giselle.donari@gmail.com>
2023-05-29 22:28:33 -0400

    update information and styles

--- src/javascript/publ.js
+++ src/javascript/publ.js
@@ -1,7 +1,37 @@
 async function getData(){
     let resp=await fetch("../data/author.json")
-    return resp
+    let data= await resp.json()
+
+    data.articles.sort((a,b)=>{return b.year-a.year})
+
+    return data
 }
 
-let x=getData()
-console.log(x)
+async function write(){
+    let data=await getData()
+    let articles=await data.articles
+
+    let listJournals=document.querySelector("#listJournals")
+    let listConferences=document.querySelector("#listConferences")
+
+    articles.forEach((article) => {
+        let li=document.createElement("li")
+        li.classList.add("mb-1")
+
+        let item=document.createElement("a")
+        item.setAttribute('href',article.link);
+        item.target="_blank"
+
+        let authorsUnderline= article.authors.replace("H Rozas", `<span class="underline">H Rozas</span>`).replace(", ...", ``)
+        let text=`${authorsUnderline}. "${article.title}", ${article.publication}.` 
+        item.innerHTML=text
+        li.append(item)
+
+        article.journal ? listJournals.append(li): listConferences.append(li)
+
+        
+
+    });
+}
+
+write()
