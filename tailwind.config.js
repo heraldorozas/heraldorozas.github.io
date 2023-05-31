3d6111aaa5d02a79c7d6864bd58311a611257b52
sweetpatry <giselle.donari@gmail.com>
2023-05-31 16:23:00 -0400

    update styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}","./**/*.html"],
+  content: ["./src/**/*.{html,js}","./index.html","./publications.html","./research.html"],
   darkMode:'class',
   theme: {
     extend: {},
